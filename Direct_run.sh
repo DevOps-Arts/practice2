2dc582577a0ba0db9cc7a8097689c98b3b262201
DevOps-Arts <107854392+DevOps-Arts@users.noreply.github.com>
2022-06-25 10:00:55 -0400

    Update Direct_run.sh

--- Direct_run.sh
+++ Direct_run.sh
@@ -1,5 +1,5 @@
 # on port 8005
 echo "Going /var/lib/jenkins/workspace/myProject/practice2"
-cd /var/lib/jenkins/workspace/myProject/practice2
+#cd /var/lib/jenkins/workspace/myProject/practice2
 echo "Runing on port 8005"
 python3 -m http.server 8005 &
